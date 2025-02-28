cf2f7913013f7940f99fdb27854bb910a65dd6c0
Ashish-web-tech <akonwtf8@gmail.com>
2025-03-01 01:10:37 +0530

    update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Parent from './components/Parent'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
     <div className='bg-black min-h-screen w-screen'>
-      <BrowserRouter>
+      <HashRouter>
         <Parent />
-      </BrowserRouter>
+      </HashRouter>
     </div>
     
     </>
